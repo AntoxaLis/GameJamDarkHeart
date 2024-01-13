7e46e11f0092b362817975ff823d487a2878b648
phi <philipephi123@gmail.com>
2024-01-13 17:26:35 -0500

    Menu

---
+++ Dark Heart/Assets/Menu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public static void boutton()
+    {
+        if (SceneManager.GetActiveScene().name != "Menu")
+        {
+            SceneManager.LoadScene("Menu");  // Chargement de la scène "jeu"
+        }
+        else
+        {
+            SceneManager.LoadScene("Show");       // Chargement de la scène 0
+        }
+    }
+    public void quitter()
+    {
+        Application.Quit();
+    }
+}
