7bf7aacc35be534f49c70e91ce26e945bb421a3f
AntoxaLis <123769950+AntoxaLis@users.noreply.github.com>
2024-01-13 15:11:49 -0500

    Moving feature

--- Dark Heart/Assets/PlayerMovement.cs
+++ Dark Heart/Assets/PlayerMovement.cs
@@ -1,20 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed = 5f;
+    public float jumpForce = 10f;
+    public Tilemap groundTilemap;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    private void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if(Input.GetKeyDown("space"))
+        // Проверка соприкосновения с землей
+        isGrounded = IsGrounded();
+
+        // Получение ввода от игрока
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        // Применение силы для движения
+        Vector2 moveVelocity = new Vector2(horizontalInput * speed, rb.velocity.y);
+        rb.velocity = moveVelocity;
+
+        // Скользящее движение при отпускании клавиши
+        if (horizontalInput == 0 && isGrounded)
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0,14,0);
+            rb.velocity = new Vector2(rb.velocity.x * 0.9f, rb.velocity.y);
         }
+
+        // Прыжок
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        Bounds bounds = GetComponent<Collider2D>().bounds;
+        Vector2 bottomLeft = new Vector2(bounds.min.x, bounds.min.y - 0.1f);
+        Vector2 bottomRight = new Vector2(bounds.max.x, bounds.min.y - 0.1f);
+
+        return groundTilemap.HasTile(groundTilemap.WorldToCell(bottomLeft)) ||
+               groundTilemap.HasTile(groundTilemap.WorldToCell(bottomRight));
     }
 }
