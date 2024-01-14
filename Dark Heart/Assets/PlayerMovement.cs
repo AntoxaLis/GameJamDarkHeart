cd3c1e19fb5f9eaa2ad85a6b76487b1b267beffa
AntoxaLis <123769950+AntoxaLis@users.noreply.github.com>
2024-01-13 21:42:49 -0500

    Adding animation (half done)

--- Dark Heart/Assets/PlayerMovement.cs
+++ Dark Heart/Assets/PlayerMovement.cs
@@ -1,54 +1,69 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Tilemaps;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5f;
-    public float jumpForce = 10f;
-    public Tilemap groundTilemap;
-
     private Rigidbody2D rb;
-    private bool isGrounded;
+    //private BoxCollider2D coll;
+    private SpriteRenderer sprite;
+    private Animator anim;
 
+    [SerializeField] private LayerMask jumpableGround;
+
+    private float dirX = 0f;
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float jumpForce = 14f;
+
+    private enum MovementState { idle, running, jumping, falling }
+
+    // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        //coll = GetComponent<BoxCollider2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+        
     }
 
+    // Update is called once per frame
     private void Update()
     {
-        // Проверка соприкосновения с землей
-        isGrounded = IsGrounded();
+        dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
 
-        // Получение ввода от игрока
-        float horizontalInput = Input.GetAxis("Horizontal");
-
-        // Применение силы для движения
-        Vector2 moveVelocity = new Vector2(horizontalInput * speed, rb.velocity.y);
-        rb.velocity = moveVelocity;
-
-        // Скользящее движение при отпускании клавиши
-        if (horizontalInput == 0 && isGrounded)
-        {
-            rb.velocity = new Vector2(rb.velocity.x * 0.9f, rb.velocity.y);
-        }
-
-        // Прыжок
-        if (isGrounded && Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump"))
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
+
+        UpdateAnimationState();
     }
 
-    private bool IsGrounded()
+    private void UpdateAnimationState()
     {
-        Bounds bounds = GetComponent<Collider2D>().bounds;
-        Vector2 bottomLeft = new Vector2(bounds.min.x, bounds.min.y - 0.1f);
-        Vector2 bottomRight = new Vector2(bounds.max.x, bounds.min.y - 0.1f);
 
-        return groundTilemap.HasTile(groundTilemap.WorldToCell(bottomLeft)) ||
-               groundTilemap.HasTile(groundTilemap.WorldToCell(bottomRight));
+        if (dirX > 0f)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = false;
+        }
+        else if (dirX < 0f)
+        {
+            anim.SetBool("running", true);     
+            sprite.flipX = true;
+        }
+        else
+        {
+            anim.SetBool("running", false);     
+        }
+
     }
+
+   /* private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
+    }*/
 }
