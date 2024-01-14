dce7fefa6f84b7375dfb2911c7e4485242a4dda9
Demonic-Gamer <demonic.keishaun@gmail.com>
2024-01-14 10:32:06 -0500

    Intruction/GameOver

---
+++ Dark Heart/Assets/MouseLossCondition.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MouseLossCondition : MonoBehaviour
+{
+    public int gameState = 0;
+
+    [SerializeField] public GameObject canvas;
+
+    [SerializeField] public GameObject intruct;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(Input.GetMouseButtonDown(0) && gameState == 0)
+        {
+
+            gameState = 1;
+
+        }
+
+        if(Input.GetMouseButtonUp(0) && gameState == 1)
+        {
+
+            gameState = 2;
+
+        }
+
+        if(gameState == 1)
+        {
+
+            Debug.Log("Playing");
+
+            intruct.SetActive(false);
+
+        }
+
+        else if(gameState == 2)
+        {
+
+            Debug.Log("Loss");
+
+            canvas.SetActive(true);
+
+        }
+
+    }
+}
